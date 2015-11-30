6d666ee50798d36dc8eb6fa5c29819039a8eb705
justinthestickman <jchuang@NN123-HA-05.HS.sd203.org>
2015-11-30 15:09:59 -0600

    Array Practice 11/30

--- Chapter 7 Practice/ArrayMethods.java
+++ Chapter 7 Practice/ArrayMethods.java
@@ -1,16 +1,36 @@
 public class ArrayMethods
 {
     private int[] values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private int currentSize = values.length;
     private int largestIndex = values.length-1;
     public ArrayMethods(int[] initialValues)
     {
-        this.values = initialValues;
+        //this.values = initialValues;
+    }
+    public void reset()
+    {
+        this.currentSize = 11;
+        for (int i = 0; i < this.values.length; i++)
+        {
+            this.values[i] = i;
+        }
+    }
+    public void printValues()
+    {
+        for (int i = 0; i < this.values.length; i++)
+        {
+            System.out.println(this.values[i]);
+        }
     }
     public void swapFirstAndLast()
     {
         int temp = this.values[0];
         this.values[0] = this.values[this.largestIndex];
         this.values[this.largestIndex] = temp;
+        for (int i = 0; i < this.values.length; i++)
+        {
+            System.out.println(this.values[i]);
+        }
     }
     public void shiftRight()
     {
@@ -20,6 +40,10 @@ public class ArrayMethods
             this.values[i] = this.values[i-1];
         }
         this.values[0] = temp;
+        for (int i = 0; i < this.values.length; i++)
+        {
+            System.out.println(this.values[i]);
+        }
     }
     public void replaceEvens()
     {
@@ -30,30 +54,49 @@ public class ArrayMethods
                 this.values[i] = 0;
             }
         }
+        for (int i = 0; i < this.values.length; i++)
+        {
+            System.out.println(this.values[i]);
+        }
     }
     public void replaceWithLargerNeighbor()
     {
-        
+        for (int i = 1; i < this.largestIndex - 1; i++)
+        {
+            
+        }
+        for (int i = 0; i < this.values.length; i++)
+        {
+            System.out.println(this.values[i]);
+        }
     }
     public void removeMiddle()
     {
         if (this.values.length % 2 == 0)
         {
-            for (int i = this.values[this.values.length / 2]; i < this.values[this.largestIndex]; i++)
+            for (int i = this.values.length / 2; i < this.values.length; i++)
             {
                 this.values[i-1] = this.values[i];
             }
         }
         else
         {
-            for (int i = this.values[this.largestIndex / 2 + 1]; i < this.values[this.largestIndex]; i++)
+            for (int i = this.largestIndex / 2 + 1; i < this.values.length; i++)
             {
                 this.values[i-1] = this.values[i];
             }
+        }
+        for (int i = 0; i < this.values.length; i++)
+        {
+            System.out.println(this.values[i]);
         }
     }
     public void moveEvens()
     {
         
+        for (int i = 0; i < this.values.length; i++)
+        {
+            System.out.println(this.values[i]);
+        }
     }
 }
--- Chapter 7 Practice/ArrayOperations.java
+++ Chapter 7 Practice/ArrayOperations.java
@@ -3,18 +3,21 @@ public class ArrayOperations
     public static void main(String[] args)
     {
         double[] x = {8, 4, 5, 21, 7, 9, 18, 2, 100};
-        System.out.println("Number of Array Items: " + x.length + "\n");
-        System.out.println("First Array Item: " + x[0] + "\n");
+        System.out.println("Number of Array Items: " + x.length);
+        System.out.println("First Array Item: " + x[0]);
         System.out.println("Last Array Item: " + x[x.length-1]);
-        System.out.println(x[x.length-1] + "\n");
+        System.out.println(x[x.length-1]);
         for (int i = 0; i < x.length; i++)
         {
-            System.out.println(x[i]);
+            System.out.println("x[" + i + "]: " + x[i]);
         }
-        System.out.println();
-        for (int i = 0; i < x.length; i++)
+        for (int i = x.length-1; i >= 0; i--)
         {
-            System.out.println();
+            System.out.println("x[" + i + "]: " + x[i]);
+        }
+        for (double value : x)
+        {
+            System.out.println(value);
         }
     }
 }

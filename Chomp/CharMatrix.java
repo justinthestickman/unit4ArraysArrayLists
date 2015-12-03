42d48766c0f9e0063be089d0695d908721a97b57
justinthestickman <jchuang@NN123-HA-05.HS.sd203.org>
2015-12-03 15:10:47 -0600

    Completed Chomp and Started Magic Squares

--- Chomp/CharMatrix.java
+++ Chomp/CharMatrix.java
@@ -24,38 +24,53 @@ public class CharMatrix
   // and fills it with the fill  character
   public CharMatrix(int rows, int cols, char fill)
   {
-    //...
+    this.grid = new char[rows][cols];
+    
+    for (int i = 0; i < this.grid.length; i++)
+    {
+        for (int j = 0; j < this.grid[i].length; j++)
+        {
+            this.grid[i][j] = fill;
+        }
+    }
   }
 
   // Returns the number of rows in grid
   public int numRows()
   {
-    //...
+    return this.grid.length;
   }
 
   // Returns the number of columns in grid
   public int numCols()
   {
-    //...
+    return this.grid[0].length;
   }
 
   // Returns the character at row, col location
   public char charAt(int row, int col)
   {
-    //...
+    return this.grid[row][col];
   }
 
   // Sets the character at row, col location to ch
   public void setCharAt(int row, int col, char ch)
   {
-    //...
+    this.grid[row][col] = ch;
   }
 
   // Returns true if the character at row, col is a space,
   // false otherwise
   public boolean isEmpty(int row, int col)
   {
-    //...
+    if (this.grid[row][col] == (' '))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
   }
 
   // Fills the given rectangle with fill  characters.
@@ -63,7 +78,16 @@ public class CharMatrix
   // lower right corner of the rectangle.
   public void fillRect(int row0, int col0, int row1, int col1, char fill)
   {
-    //...
+    for (int i = 0; i < this.grid.length; i++)
+    {
+        for (int j = 0; j < this.grid[i].length; j++)
+        {
+            if (i >= row0 && i <= row1 && j >= col0 && j <= col1)
+            {
+                this.grid[i][j] = fill;
+            }
+        }
+    }
   }
 
   // Fills the given rectangle with SPACE characters.
@@ -71,18 +95,43 @@ public class CharMatrix
   // lower right corner of the rectangle.
   public void clearRect(int row0, int col0, int row1, int col1)
   {
-    //...
+    for (int i = 0; i < this.grid.length; i++)
+    {
+        for (int j = 0; j < this.grid[i].length; j++)
+        {
+            if (i >= row0 && i <= row1 && j >= col0 && j <= col1)
+            {
+                this.grid[i][j] = ' ';
+            }
+        }
+    }
   }
 
   // Returns the count of all non-space characters in row 
   public int countInRow(int row)
   {
-    //...
+    int counter = 0;
+    for (int j = 0; j < this.grid[row].length; j++)
+    {
+        if (this.grid[row][j] != ' ')
+        {
+            counter++;
+        }
+    }
+    return counter;
   }
 
   // Returns the count of all non-space characters in col 
   public int countInCol(int col)
   {
-    //...
+    int counter = 0;
+    for (int i = 0; i < this.grid.length; i++)
+    {
+        if (this.grid[i][col] != ' ')
+        {
+            counter++;
+        }
+    }
+    return counter;
   }
 }

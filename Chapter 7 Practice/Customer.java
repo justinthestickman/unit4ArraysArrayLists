82116986933a3f266d0a702443f95a8e26007fed
justinthestickman <jchuang@NN123-HA-05.HS.sd203.org>
2015-12-07 15:04:03 -0600

    Finished Chapter 7 Practice

--- Chapter 7 Practice/Customer.java
+++ Chapter 7 Practice/Customer.java
@@ -1,8 +1,20 @@
 public class Customer
 {
-    
-    public Customer()
+    private String name;
+    private double sale;
+    public Customer(String customerName, double amount)
     {
-        
+        this.name = customerName;
+        this.sale = amount;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public double getAmount()
+    {
+        return this.sale;
     }
 }
--- Chapter 7 Practice/Store.java
+++ Chapter 7 Practice/Store.java
@@ -3,17 +3,46 @@ import java.util.Scanner;
 
 public class Store
 {
-    private ArrayList<Customer> customer = new ArrayList<Customer>();
-    
-    
+    private ArrayList<Customer> customers = new ArrayList<Customer>();
     public void addSale(String customerName, double amount)
     {
-        
+        Customer customer = new Customer(customerName, amount);
+        customers.add(customer);
     }
     
     public String nameOfBestCustomer()
     {
-        
-        return "asdf";
+        Customer bestCustomer = new Customer(" ",0);
+        double largestSale = 0;
+        for (Customer customer : customers)
+        {
+            double sale = customer.getAmount();
+            if (sale > largestSale)
+            {
+                largestSale = sale;
+                bestCustomer = customer;
+            }
+        }
+        return bestCustomer.getName();
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Store store = new Store();
+        String name;
+        double price;
+        do
+        {
+            System.out.print("Name of Customer and Price (separated by a space): ");
+            name = scanner.next();
+            price = scanner.nextDouble();
+            if (price != 0)
+            {
+                store.addSale(name,price);
+            }
+        }
+        while (price != 0);
+        System.out.println("Best Customer: " + store.nameOfBestCustomer());
     }
 }
